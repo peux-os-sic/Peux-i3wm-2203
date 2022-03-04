e0e7a4a6a17b0455a2aae1a36628c5eb132a34f7
DN-debug <75267055+DN-debug@users.noreply.github.com>
2022-03-04 10:39:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ then
     echo "changing directory"
     cd ../
     mkdir $HOME/tmp
-    cp -r Peux-River/ $HOME/tmp/
+    cp -r Peux-i3wm-2203/ $HOME/tmp/
     rm -rf $HOME/tmp/Peux-i3wm-2203/install.sh
     rm -rf $HOME/tmp/Peux-i3wm-2203/.git
     rm -rf $HOME/tmp/Peux-i3wm-2203/details.md
